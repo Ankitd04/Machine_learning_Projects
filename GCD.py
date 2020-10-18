a1351a41bfddc8f83a078b09531effe509be0acf
Ankit Dhakate <ankitsad04@gmail.com>
2020-10-18 12:41:38 +0530

    first version of GCD code

---
+++ GCD.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter 1st number"))
+num2 = int(input("Enter 2nd number"))
+
+def get_gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return get_gcd(b, a % b)
+
+gcd = get_gcd(num1, num2)
+print("GCD of {0} and {1} is : {2}".format(num1, num2, gcd))
